94f7167fd26bb6cb7de9ef2308fd3356747ab7b8
Kolton Starr <starrkolton@gmail.com>
2019-05-22 18:07:11 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,69 @@
-# your code goes here
+def begins_with_r(array)
+  tf_arr = []
+  array.each do |elmt|
+    if elmt.start_with?("r")
+      tf_arr << true 
+    else 
+      tf_arr << false 
+
+
+    end 
+  
+  end 
+  if tf_arr.include?(false)
+    false 
+  else 
+    true 
+
+  end 
+end
+
+def contain_a(array)
+arr = []
+  array.each do |str|
+    if str.include?('a')
+      arr << str 
+
+
+
+    end 
+  end 
+  arr 
+end
+
+def first_wa(array)
+  array.find do |elmt|
+    if elmt.class == String
+      elmt.start_with?("wa")
+
+      
+    end 
+  end
+end 
+
+def remove_non_strings(array)
+  clean_arr = []
+  array.each do |elmt|
+    if elmt.class == String 
+      clean_arr << elmt
+
+    end 
+  end 
+  clean_arr
+end
+
+def count_elements(array)
+  counts = Hash.new(0)
+  array.each { |name| counts[name] += 1 }
+
+
+  counts 
+  
+end
+
+
+
+
+
+
+
